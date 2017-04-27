7e7433a3aff056b303f78294ae9d52ab7a683d14
Shyngys Zhiyenbek <kitamrofni@gmail.com>
2017-04-27 14:10:59 +0600

    Deleted unused imports

--- src/classes/Environment.java
+++ src/classes/Environment.java
@@ -6,11 +6,9 @@
 package classes;
 
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import static java.lang.Integer.max;
 import static java.lang.Integer.min;
